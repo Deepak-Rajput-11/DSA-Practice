a9729de025838eb4f3cb8a4cb20f03f65c43ecf4
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-04-12 14:41:00 +0530

    Perform left rotation with extra array

---
+++ LeftRotationWithArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    int k = 2;
+    int arr[n] = {1, 2, 3, 4, 5};
+    int New[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        New[(i - k + n) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << New[i] << " ";
+    }
+
+    return 0;
+}
