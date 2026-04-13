c0b6fc2de6e8743d3b981016bb65bd642dd535cb
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-04-13 23:39:04 +0530

    Perform kadane algorithm

---
+++ KadaneAlgorithm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+    int currentSum = 0;
+    int maxSum = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        currentSum += arr[i];
+        if (currentSum > maxSum)
+        {
+            maxSum = currentSum;
+        }
+        if (currentSum < 0)
+        {
+            currentSum = 0;
+        }
+    }
+    cout << maxSum;
+    return 0;
+}
