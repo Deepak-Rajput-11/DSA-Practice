e2a7b41e2d668451c9acf7cc0908352537577214
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-08-10 06:16:01 +0530

    Done with Equilibrium index

--- EquililbriumIndex.cpp
+++ EquililbriumIndex.cpp
@@ -1,26 +1,55 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[]{1, 3, 5, 2, 2};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     for (int i = 0; i < n; i++)
+//     {
+//         int LeftSum = 0;
+//         int RightSum = 0;
+//         for (int j = 0; j < i; j++)
+//         {
+//             LeftSum += arr[j];
+//         }
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             RightSum += arr[j];
+//         }
+//         if (LeftSum == RightSum)
+//         {
+//             cout << "Equilibrium index: " << i;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+//
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int arr[]{1, 3, 5, 2, 2};
+    int arr[] = {1, 3, 5, 2, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
+    int totalsum = 0;
     for (int i = 0; i < n; i++)
     {
-        int LeftSum = 0;
-        int RightSum = 0;
-        for (int j = 0; j < i; j++)
+        totalsum += arr[i];
+    }
+    int leftsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        int rightsum = totalsum - leftsum - arr[i];
+
+        if (leftsum == rightsum)
         {
-            LeftSum += arr[j];
-        }
-        for (int j = i + 1; j < n; j++)
-        {
-            RightSum += arr[j];
-        }
-        if (LeftSum == RightSum)
-        {
-            cout << "Equilibrium index: " << i;
+            cout << "Equilbrium index: " << i;
             break;
         }
+        leftsum += arr[i];
     }
     return 0;
 }
