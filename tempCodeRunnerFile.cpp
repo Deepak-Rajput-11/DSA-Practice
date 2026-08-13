dcbf4f88306dfb359b3b28d7dcce374d0b840c4c
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-08-13 13:28:05 +0530

    Trying again some problem with different value

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,24 @@
-for (int i = 0; i < n; i++)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {2, 3, -8, 7, -1, 2, 3};
+    int sum = 0;
+    int max = arr[0];
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++)
     {
-        cin >> arr[i];
+        sum += arr[i];
+        if (sum > max)
+        {
+            max = sum;
+        }
+        if (sum < 0)
+        {
+            sum = 0;
+        }
     }
+    cout << max;
+    return 0;
+}
