9da0b3a4503df6df8bada6e4fd79b88dbf43cfd8
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-08-16 21:26:04 +0530

    Practising some question again

--- Practice.cpp
+++ Practice.cpp
@@ -53,27 +53,62 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+// int main()
+// {
+//     int arr[] = {12, 5, 271, 8, 919};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     int smallest = arr[0];
+//     int secondsmallest = INT_MAX;
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] < smallest)
+//         {
+//             secondsmallest = smallest;
+//             smallest = arr[i];
+//         }
+//         else if (arr[i] > smallest && arr[i] < secondsmallest)
+//         {
+//             secondsmallest = arr[i];
+//         }
+//     }
+//     cout << secondsmallest;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     int temp = arr[0];
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         arr[i] = arr[i + 1];
+//     }
+//     arr[n - 1] = temp;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << " " << arr[i];
+//     }
+//     return 0;
+// }
+
 #include <iostream>
-#include <climits>
 using namespace std;
 int main()
 {
-    int arr[] = {12, 5, 271, 8, 919};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int largest = arr[0];
-    int secondLargest = INT_MIN;
-    for (int i = 0; i < size; i++)
+    int arr[] = {16, 17, 4, 3, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n - 1; i <= 0; i--)
     {
-        if (arr[i] > largest)
+        if (arr[i] < arr[i - 1])
         {
-            secondLargest = largest;
-            largest = arr[i];
-        }
-        else if (arr[i] < largest && arr[i] > secondLargest)
-        {
-            secondLargest = arr[i];
+            cout << arr[i];
         }
     }
-    cout << secondLargest;
     return 0;
 }
