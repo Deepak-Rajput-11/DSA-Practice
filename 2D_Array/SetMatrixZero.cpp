96def2fc372a19690f6212fcb478660648ec82be
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-09-03 07:11:18 +0530

    set marked row and column of marked matrix to zeros

---
+++ 2D_Array/SetMatrixZero.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3][3] = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}};
+
+    int zeroRow[3] = {0};
+    int zeroCol[3] = {0};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr[i][j] == 0)
+            {
+                zeroRow[i] = 1;
+                zeroCol[j] = 1;
+            }
+            if (zeroRow[i] == 1 || zeroCol[j] == 1)
+            {
+                arr[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (zeroRow[i] == 1 || zeroCol[j] == 1)
+            {
+                arr[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
