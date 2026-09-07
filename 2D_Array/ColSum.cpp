520029e5f2eded5929b47c57ebfa70e890b0cbb7
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-09-07 23:57:06 +0530

    sum of columns

--- 2D_Array/ColSum.cpp
+++ 2D_Array/ColSum.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main()
 {
-    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int arr[3][3] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9}};
     for (int j = 0; j < 3; j++)
     {
         int sum = 0;
