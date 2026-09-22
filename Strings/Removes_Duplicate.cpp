1d38cab2afe348c59fe0c5f0f6e360d524314d7d
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-09-22 06:44:10 +0530

    Better approach for removes duplicates

--- Strings/Removes_Duplicate.cpp
+++ Strings/Removes_Duplicate.cpp
@@ -1,27 +1,48 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string str = "programming";
+//     int n = str.length();
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         bool duplicate = false;
+//         for (int k = 0; k < i; k++)
+//         {
+//             if (str[i] == str[k])
+//             {
+//                 duplicate = true;
+//                 break;
+//             }
+//         }
+//         if (duplicate == true)
+//         {
+//             continue;
+//         }
+
+//         cout << str[i];
+//     }
+
+//     return 0;
+// }
+
+// Better Approach
+
 #include <iostream>
 using namespace std;
 int main()
 {
     string str = "programming";
-    int n = str.length();
-
-    for (int i = 0; i < n; i++)
+    int freq[256] = {0};
+    for (int i = 0; i < str.length(); i++)
     {
-        bool duplicate = false;
-        for (int k = 0; k < i; k++)
-        {
-            if (str[i] == str[k])
-            {
-                duplicate = true;
-                break;
-            }
-        }
-        if (duplicate == true)
-        {
-            continue;
-        }
+        freq[str[i]]++;
 
-        cout << str[i];
+        if (freq[str[i]] == 1)
+        {
+            cout << str[i];
+        }
     }
 
     return 0;
