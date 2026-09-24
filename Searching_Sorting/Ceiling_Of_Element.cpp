93ecf3aea76ac23eccf9436c571731d4b42da389
Deepak-Rajput-11 <rajputdeepak2704@gmail.com>
2026-09-24 09:12:18 +0530

    Find the ceiling of the element

---
+++ Searching_Sorting/Ceiling_Of_Element.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 4, 6, 8, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 9;
+    int start = 0;
+    int end = n - 1;
+    int get = -1;
+    bool found = false;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+        {
+            get = arr[mid];
+            found = true;
+            break;
+        }
+        else if (arr[mid] >= target)
+        {
+            found = true;
+            get = arr[mid];
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    if (found)
+    {
+        cout << "Ceiling: " << get;
+    }
+    return 0;
+}
